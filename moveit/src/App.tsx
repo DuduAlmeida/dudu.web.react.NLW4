5ec4ccc4fa9a1998f8952eebe25e31d3bffcfb99
Eduardo Almeida <eduardo.oliveira@facens.br>
2021-03-15 16:33:34 -0300

    feat(aula1): ended tasks of the first lesson

---
+++ moveit/src/App.tsx
@@ -0,0 +1,17 @@
+/* #region Imports*/
+
+import ExperienceBar from './components/ExperienceBar/ExperienceBar';
+
+import './styles/global.css';
+
+/* #endregion Imports*/
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar></ExperienceBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ moveit/src/index.tsx
@@ -0,0 +1,15 @@
+/* #region Imports*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+/* #endregion Imports*/
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
